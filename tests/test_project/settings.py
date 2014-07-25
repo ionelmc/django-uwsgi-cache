c9bfc162481a918201492d3dc75d275ae3354be7
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-07-25 20:03:13 +0300

    Use ephemeral db.

--- tests/test_project/settings.py
+++ tests/test_project/settings.py
@@ -45,10 +45,10 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
 }
 
 # Internationalization
