ea1308c07c3f14c36a757f4f8bfdf08f75be8d10
Ionel Cristian Mărieș <contact@ionelmc.ro>
2025-10-01 16:00:02 +0300

    Cleanup test settings.

--- tests/test_project/settings.py
+++ tests/test_project/settings.py
@@ -1,44 +1,11 @@
-"""
-Django settings for test_project project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/dev/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/dev/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "+ln!mmswhbemdn@*v8sbic_n+i&j4+ct8(n=y09s81c)7fyyf2"  # noqa:S105
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
+DEBUG = TEMPLATE_DEBUG = False
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 INSTALLED_APPS = ("test_app",)
-
-MIDDLEWARE_CLASSES = ()
-
 ROOT_URLCONF = "test_app.urls"
-
 WSGI_APPLICATION = "test_project.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/dev/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -46,17 +13,31 @@ DATABASES = {
     }
 }
 
-# Internationalization
-# https://docs.djangoproject.com/en/dev/topics/i18n/
-
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/dev/howto/static-files/
-
-STATIC_URL = "/static/"
-
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "[%(asctime)s.%(msecs)d] %(name)s (%(levelname)s) %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "root": {
+        "level": "DEBUG",
+        "handlers": ["console"],
+    },
+    "loggers": {
+        "django.request": {"level": "DEBUG"},
+        "django.db.backends": {"level": "DEBUG"},
+    },
+    "filters": {},
+}
 CACHES = {"default": {"BACKEND": "uwsgicache.UWSGICache", "LOCATION": "foobar"}}
 UWSGI_CACHE_FALLBACK = os.getenv("UWSGI_CACHE_FALLBACK", False)
